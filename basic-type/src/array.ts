6b283780d6312732931feb92adf29cea19c40761
anamolhasan <mdanamolhasan1998@gmail.com>
2025-09-06 23:04:30 +0600

    tuple

--- basic-type/src/array.ts
+++ basic-type/src/array.ts
@@ -8,3 +8,7 @@ let friend : string[] = ['anam', 'sabbir', 'maruf']
 // alternative way to declare an array
 let scores2: Array <number> = [2,3,4,5,6]
 console.log(scores[0])
+
+// tuple
+let student:[string,number,boolean]=['anam',27,true]
+console.log(student[0])
