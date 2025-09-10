7d52725ec02724f96fef2a9a92d8e446a7395f1f
anamolhasan <mdanamolhasan1998@gmail.com>
2025-09-10 16:51:15 +0600

    optional change

--- function/src/objectAliases.ts
+++ function/src/objectAliases.ts
@@ -12,3 +12,36 @@ function printUser (user:{name:string,age:number}) : void {
 printUser((student))
 
 
+
+// type alias
+type User = {
+    name:string,
+    age:number
+}
+
+function showUser (user: User) {
+    console.log(user.name)
+}
+const user1 = {name:'hasan', age:23}
+showUser(user1)
+
+
+type Product = {
+    id:number,
+    name:string,
+    price:number,
+    description?:string    // optional change ?
+}
+const item1:Product = {
+     id:1,
+     name:'laptop',
+     price:29000,
+
+}
+const item2:Product = {
+     id:1,
+     name:'m',
+     price:29000,
+     description:'how mony much very something'
+
+}
