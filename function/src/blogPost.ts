e9df2f9241f278f01382ed0e320d77ddc76d016b
anamolhasan <mdanamolhasan1998@gmail.com>
2025-09-10 19:28:29 +0600

    blog post

---
+++ function/src/blogPost.ts
@@ -0,0 +1,29 @@
+// define type alias
+
+type BlogPost = {
+    title:string,
+    content:string,
+    author:string,
+    published:boolean,
+    publishedDate?:string
+}
+
+// create a blog post
+
+const myPost : BlogPost = {
+    title:'Learn Typescript',
+    content:'Typescript is a superset of javascript add that adds static typing.',
+    author:'anamol hasan',
+    published:true,
+    publishedDate:'2025 - 09 - 09'
+}
+
+
+// crete a function to display the blog post 
+function printPost(post:BlogPost){
+  console.log(`Title : ${post.title}`)
+  console.log(`Title : ${post.author}`)
+  console.log(`Title : ${post.content}`)
+}
+
+printPost(myPost)
