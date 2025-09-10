b640b2429352050b7b113f551b115aab1cfcd911
anamolhasan <mdanamolhasan1998@gmail.com>
2025-09-10 20:20:28 +0600

    type vs interface

---
+++ function/src/aliasInterface.ts
@@ -0,0 +1,38 @@
+
+
+// type alias
+type User = {
+    name:string,
+    age:number,
+    email:string
+}
+
+// define interface
+
+interface UserProfile {
+    name:string,
+    age:number,
+    email:string
+}
+
+// marge the interface of a Parson
+
+interface Parson {
+    name:string
+}
+
+interface Parson {
+    age:number
+}
+
+const p : Parson = {name:'anamol hasan', age:27}
+
+
+// when to use which
+
+
+// 1. designing public APIs : interface
+// 2. for simple object use both 
+// 3. Union and Intersection types : use type 
+// 4. primitive or function : use type 
+// 5. flexibility: interface (extended) and type (union and interceptor)
