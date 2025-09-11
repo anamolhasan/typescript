28bc6df4a8b0f7f22f71d97502d7ed8ddc287f96
anamolhasan <mdanamolhasan1998@gmail.com>
2025-09-11 08:07:16 +0600

    user profile project

---
+++ function/src/user_profile.ts
@@ -0,0 +1,30 @@
+// user profile
+
+// step 1. create an user interface
+interface User {
+    id:number,
+    userName:string,
+    email:string
+}
+
+// step 2. create an author interface
+interface Author extends User {
+    bio:string,
+    posts:string[]
+}
+
+// step 3. define user status using type
+type Status = 'active' | 'inactive' | 'banned'
+
+
+// step 4. crete an author object
+const author1 : Author = {
+    id:1,
+    userName:'anamol hasan',
+    email:'anamolhasan.job@gmail.com',
+    bio:'I am a full stack developer',
+    posts:['post 1', 'post 2', 'post 3']
+}
+
+// step 4. make account status
+const accountStatus : Status = 'active'
