1f177968bcd1b817fcdd092d178c054b0fa5ec03
anamolhasan <mdanamolhasan1998@gmail.com>
2025-10-21 17:24:20 +0600

    generics  with interface learn

---
+++ generics/src/function-interfaces.ts
@@ -0,0 +1,28 @@
+//  with generics (super reusable)
+
+function logData <T>(data: T) {
+    console.log(data)
+}
+logData<string>("Hello")
+logData<number>(222)
+logData<boolean>(true)
+
+
+
+// generics interface example
+interface ApiResponse<T> {
+    success:boolean;
+    data: T
+}
+
+const userResponse:ApiResponse<{name:string}> = {
+     success:true,
+     data: {
+        name: "Alice"
+     }
+}
+
+const numberResponse: ApiResponse<number> = {
+    success: false,
+    data:42
+}
